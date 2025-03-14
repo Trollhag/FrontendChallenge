31c2dab1fc9b1c42a198c81be6e48aab7f41c466
Oscar Trollhag <oscar@trollhag.com>
2025-03-14 14:32:23 +0100

    feat: started countdown app, added input fields

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router'
+import { CountdownComponent } from './routes/countdown/countdown.component'
 
-export const routes: Routes = []
+export const routes: Routes = [{ path: '', component: CountdownComponent }]
